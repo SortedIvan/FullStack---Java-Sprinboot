6198aee3b3dfc55429833749156630c912a4c099
Ovcharov,Ivan <450754@student.fontys.nl>
2021-11-05 21:46:14 +0200

    new database export

--- Database Export/gamewebshop.sql
+++ Database Export/gamewebshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2021 at 09:12 PM
+-- Generation Time: Nov 05, 2021 at 08:45 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -40,9 +40,9 @@ CREATE TABLE `app_user` (
 --
 
 INSERT INTO `app_user` (`id`, `email`, `name`, `password`, `username`) VALUES
-(4, 'ivan@gmail.com', 'Ivan Ovcharov', '$2a$10$DTLr27ttYMrBxOxgkbjyEuxDWDjW/qWsDVVAJ6ZZpMtLNQWJOpc1e', 'ivan2e'),
-(5, 'tom123@gmail.com', 'Hamako Yutai', '$2a$10$09EopZCcdUeM76yzeSxwTeCfOUGa4qP6.NgsMISRO7KW9IF2Og14i', 'hamako'),
-(6, 'boriz@gmail.com', 'Boriz Tunai', '$2a$10$bY730HJwnTsvF.T7.QRyouJVNVqbdLOvqe.iw3xFf.wMz5rqMQUgG', 'boriz1');
+(4, 'ivan@gmail.com', 'Ivan Ovcharov', '$2a$10$yDcYXlnC3e/UlZZd/tLPmurDvCRzSBZz6p1.4u/l7pIlCGwQ7tOnS', 'ivan2e'),
+(5, 'tom123@gmail.com', 'Hamako Yutai', '$2a$10$pVgJ0mWx.hf57.G5bf6A8eJYk81I.lkoTyV5yx5GEdcQYOjwFV52m', 'hamako'),
+(6, 'boriz@gmail.com', 'Boriz Tunai', '$2a$10$I41PU3ASdZddx55DpN6ndO/UVB8VmTIS.mnlRFMeo34uFwV1N/GDe', 'boriz1');
 
 -- --------------------------------------------------------
 
@@ -93,6 +93,53 @@ INSERT INTO `game` (`id`, `game_name`, `game_price`, `game_rating`, `game_size`)
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `games`
+--
+
+CREATE TABLE `games` (
+  `id` bigint(20) NOT NULL,
+  `gamename` varchar(255) DEFAULT NULL,
+  `gameplaytype` int(11) DEFAULT NULL,
+  `gameprice` double DEFAULT NULL,
+  `gamesize` double DEFAULT NULL,
+  `app_user_id` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `games`
+--
+
+INSERT INTO `games` (`id`, `gamename`, `gameplaytype`, `gameprice`, `gamesize`, `app_user_id`) VALUES
+(13, 'Mario', 0, 19.99, 500, 7),
+(14, 'Zelda', 1, 29.99, 400, 7),
+(15, 'AdventureGame', 0, 39.99, 900, 7),
+(16, 'Rust', 1, 59.99, 1000, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games_game_types`
+--
+
+CREATE TABLE `games_game_types` (
+  `game_id` bigint(20) NOT NULL,
+  `game_types_id` bigint(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `games_game_types`
+--
+
+INSERT INTO `games_game_types` (`game_id`, `game_types_id`) VALUES
+(14, 11),
+(13, 10),
+(15, 12),
+(15, 9),
+(16, 11);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `game_game_types`
 --
 
@@ -115,6 +162,29 @@ INSERT INTO `game_game_types` (`game_id`, `game_types_id`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `game_rating`
+--
+
+CREATE TABLE `game_rating` (
+  `ratingid` bigint(20) NOT NULL,
+  `rating` double NOT NULL,
+  `game_id` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game_system_requirements`
+--
+
+CREATE TABLE `game_system_requirements` (
+  `game_id` bigint(20) NOT NULL,
+  `system_requirements` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `hibernate_sequence`
 --
 
@@ -127,7 +197,7 @@ CREATE TABLE `hibernate_sequence` (
 --
 
 INSERT INTO `hibernate_sequence` (`next_val`) VALUES
-(16);
+(17);
 
 -- --------------------------------------------------------
 
@@ -165,11 +235,69 @@ CREATE TABLE `type_game` (
 --
 
 INSERT INTO `type_game` (`id`, `name`) VALUES
-(7, 'ARCADE'),
-(8, 'HORROR'),
-(9, 'SCI_FI'),
-(10, 'ADVENTURE'),
-(11, 'SURVIVAL');
+(8, 'ARCADE'),
+(9, 'HORROR'),
+(10, 'SCI_FI'),
+(11, 'ADVENTURE'),
+(12, 'SURVIVAL');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `name`, `password`, `username`) VALUES
+(4, 'ivan888@gmail.com', 'Ivan Ovcharov', '$2a$10$ApSLQBNICsosa9mgQRsfUOZk1aMWslsRa4698dsj5n3esRYlpZNji', 'ivan2e'),
+(5, 'tom123@gmail.com', 'Hamako Yutai', '$2a$10$bYJXZBWjlARE1Bs6wcdl4.ldQaQoq0c9bdFoQcrM7VBdCcwwaGQ1W', 'hamako'),
+(6, 'boriz@gmail.com', 'Boriz Tunai', '$2a$10$AiU/cOAn/8lvlcOXc5eEY.hRlOwBQnihGvAMNj70sUH55zQkUNyke', 'boriz1'),
+(7, 'ivan@gmail.com', 'Ivan Test', '$2a$10$ta14B9eVuyzPmO.4moxNlOROFVFNb.CPJsror.PZOymtOsZXjRhxe', 'ivantest');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_gameids`
+--
+
+CREATE TABLE `users_gameids` (
+  `app_user_id` bigint(20) NOT NULL,
+  `gameids_id` bigint(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_roles`
+--
+
+CREATE TABLE `users_roles` (
+  `app_user_id` bigint(20) NOT NULL,
+  `roles_id` bigint(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users_roles`
+--
+
+INSERT INTO `users_roles` (`app_user_id`, `roles_id`) VALUES
+(4, 1),
+(5, 2),
+(6, 2),
+(6, 3),
+(6, 1),
+(7, 3);
 
 --
 -- Indexes for dumped tables
@@ -195,11 +323,38 @@ ALTER TABLE `game`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKb0kpgf4yas7dgsmrdvqv65pge` (`app_user_id`);
+
+--
+-- Indexes for table `games_game_types`
+--
+ALTER TABLE `games_game_types`
+  ADD KEY `FKsfhaoyo6ldso4uejxf73kdt4h` (`game_types_id`),
+  ADD KEY `FKf7do7fpdctgnhk0vom00g8tu0` (`game_id`);
+
+--
 -- Indexes for table `game_game_types`
 --
 ALTER TABLE `game_game_types`
   ADD KEY `FK4ewe6v9wk5sanx5uw0k5objof` (`game_types_id`),
   ADD KEY `FKe2tc4ihloiwcu63dsyi7of5xv` (`game_id`);
+
+--
+-- Indexes for table `game_rating`
+--
+ALTER TABLE `game_rating`
+  ADD PRIMARY KEY (`ratingid`),
+  ADD KEY `FKlshfk3wvme0j53er2a480gty3` (`game_id`);
+
+--
+-- Indexes for table `game_system_requirements`
+--
+ALTER TABLE `game_system_requirements`
+  ADD KEY `FK10c1qp5g6e6k3xjw6w005c8x` (`game_id`);
 
 --
 -- Indexes for table `role`
@@ -212,6 +367,27 @@ ALTER TABLE `role`
 --
 ALTER TABLE `type_game`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UK_6dotkott2kjsp8vw4d0m25fb7` (`email`) USING HASH;
+
+--
+-- Indexes for table `users_gameids`
+--
+ALTER TABLE `users_gameids`
+  ADD UNIQUE KEY `UK_5oxnfnuwbcm8piy15pvkoacs8` (`gameids_id`),
+  ADD KEY `FKjxqri8nx7718ya58cyksyjpga` (`app_user_id`);
+
+--
+-- Indexes for table `users_roles`
+--
+ALTER TABLE `users_roles`
+  ADD KEY `FK15d410tj6juko0sq9k4km60xq` (`roles_id`),
+  ADD KEY `FKar2y0lww0xn3x3aoqfg9qsgr5` (`app_user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
